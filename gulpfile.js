e4fbac26a046a7adaacc21d383b42416b9e96a76
cj <thatcat13@gmail.com>
2018-01-05 08:58:13 -0800

    add gulp tasks and dependencies that include uglifying JS files and setting up a build environment and deleting temporary folders and error checking

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,19 @@ var source = require('vinyl-source-stream');
 //vinyl is used for putting browserified source code into a new file
 var concat = require('gulp-concat');
 //combines interface JS files together into one monster interface.js file
-
+var uglify = require('gulp-uglify');
+//minifies JS to readable code for the computer with the aim of streamlining the app
+var utilities = require('gulp-util');
+//sets up build environment that works with the buildProduction dependencies
+var del = require('del');
+//removes files within asset pipeline when no longer needed; build and tmp folders will need to be fresh and empty when beginning development
+var jshint = require('gulp-jshint');
+//looks for errors in JS files
+var buildProduction = utilities.env.production;
+//buildProduction is an environment variable
+//indicates which environment is being used (dev or production)
+//$ gulp build --production: sets var to true, thus production build
+//$gulp build: sets var to false, thus development build
 
 //TASKS
 
@@ -40,3 +52,37 @@ gulp.task('jsBrowserify', ['concatInterface'], function(){
 //put it into a new folder named build inside its own js folder
 });
 //build folder is used to separate out production version of the project
+
+//MINIFY TASK
+//(after jsBrowserify task b/c jsBrowserify is a dependency of the minification task)
+gulp.task('minifyScripts', ['jsBrowserify'], function(){
+  return gulp.src('./build/js/app.js')
+  .pipe(uglify())
+  .pipe(gulp.dest('./build/js'));
+});
+//so when minifyScripts task is run, it will run jsBrowserify, which will run concatInterface
+
+//CLEAN TASK
+gulp.task('clean', function(){
+  return del(['build', 'tmp']);
+});
+//pass del function an array of paths to delete and it removes them
+//clean task is called automatically by making it a dependency of the build task
+
+//BUILD TASK
+gulp.task('build', ['clean'], function(){
+  if (buildProduction) {
+    gulp.start('minifyScripts');
+  } else {
+    gulp.start('jsBrowserify')
+  }
+});
+//will make a fresh folder of the newest files to work with
+
+//LINTING TASK
+gulp.task('jshint', function(){
+  return gulp.src(['js/*.js'])
+  .pipe(jshint())
+  .pipe(jshint.reporter('default'));
+});
+//this will pull in all the js files in js folder, run jshint on them and report back any errors found

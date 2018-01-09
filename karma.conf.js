e0651fdc444881ae34b2387032f1a83ae7ea78a8
cj <thatcat13@gmail.com>
2018-01-08 18:33:44 -0800

    restore karmaconf file

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,5 @@
 // Karma configuration
-// Generated on Mon Jan 08 2018 18:17:57 GMT-0800 (PST)
+// Generated on Fri Jan 05 2018 09:40:55 GMT-0800 (PST)
 
 module.exports = function(config) {
   config.set({
@@ -10,11 +10,13 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine'],
+    frameworks: ['jquery-3.2.1','jasmine', 'jasmine-matchers', 'browserify'],
 
 
     // list of files / patterns to load in the browser
     files: [
+      'js/*.js',
+      'spec/*-spec.js',
     ],
 
 
@@ -26,13 +28,28 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      'js/*.js': [ 'browserify'],
+      'spec/*.js': ['browserify'],
+    },
+
+    plugins: [
+      'karma-jquery',
+      'karma-browserify',
+      'karma-jasmine',
+      'karma-jasmine-matchers',
+      'karma-chrome-launcher',
+      'karma-jasmine-html-reporter'
+    ],
+    browserify: {
+      debug: true,
+      transform: [ [ 'babelify', {presets: ["es2015"]} ] ]
     },
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'kjhtml'],
 
 
     // web server port
